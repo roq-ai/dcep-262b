e94b9e2a922349960c2eae4aa3b8f13443fe972a
roq-ai <>
2023-07-08 13:03:18 +0000

    Initial commit

---
+++ src/interfaces/project/index.ts
@@ -0,0 +1,24 @@
+import { TaskInterface } from 'interfaces/task';
+import { OrganizationInterface } from 'interfaces/organization';
+import { GetQueryInterface } from 'interfaces';
+
+export interface ProjectInterface {
+  id?: string;
+  name: string;
+  status: string;
+  organization_id?: string;
+  created_at?: any;
+  updated_at?: any;
+  task?: TaskInterface[];
+  organization?: OrganizationInterface;
+  _count?: {
+    task?: number;
+  };
+}
+
+export interface ProjectGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  name?: string;
+  status?: string;
+  organization_id?: string;
+}
